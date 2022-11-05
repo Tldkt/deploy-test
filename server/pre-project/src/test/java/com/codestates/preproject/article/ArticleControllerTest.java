01f7f497f0cab11e1b065a7f4297e93212b352b3
Tldkt <evolym2543@sookmyung.ac.kr>
2022-11-05 17:43:56 +0900

    merge deploy

---
+++ server/pre-project/src/test/java/com/codestates/preproject/article/ArticleControllerTest.java
@@ -0,0 +1,16 @@
+/*
+package com.codestates.preproject.article;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleControllerTest {
+
+    @Test
+    @DisplayName("게시글 하나 조회")
+    void getArticle() {
+
+    }
+}*/
